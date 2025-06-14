b213c6854d96fd9cc227f460516fa922bed35b47
Viktor Stepanenko <vitya.stepanenko@gmail.com>
2025-06-14 23:45:48 +0100

    Task 1: Add S3 bucket resource, variables file, and Terraform GitHub Actions pipeline

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+  versioning {
+    enabled = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  description = "S3 bucket name for Terraform state"
+  type        = string
+}
